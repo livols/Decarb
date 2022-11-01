820a7249bfb748948a80b17055895f14097aef43
livolsen <livolsen@live.com>
2022-11-01 08:45:57 +0100

    Changed data and some changes done to stranding diagram

--- src/visualisation/strandingDiagram.tsx
+++ src/visualisation/strandingDiagram.tsx
@@ -12,51 +12,99 @@ import {
 const data = [
   {
     Year: "2020",
-    "Baseline carbon performance of asset": 90,
-    "Sector decarbonisation target": 100,
-    "Constant emission": 90,
-  },
-  {
-    Year: "2021",
-    "Baseline carbon performance of asset": 80,
-    "Sector decarbonisation target": 80,
-    "Constant emission": 90,
+    "Baseline carbon performance of asset": 75,
+    "Sector decarbonisation target": 95,
+    "Constant emission": 75,
   },
   {
     Year: "2022",
-    "Baseline carbon performance of asset": 45,
-    "Sector decarbonisation target": 60,
-    "Constant emission": 90,
-  },
-  {
-    Year: "2023",
-    "Baseline carbon performance of asset": 40,
-    "Sector decarbonisation target": 40,
-    "Constant emission": 90,
+    "Baseline carbon performance of asset": 75,
+    "Sector decarbonisation target": 98,
+    "Constant emission": 75,
   },
   {
     Year: "2024",
-    "Baseline carbon performance of asset": 10,
-    "Sector decarbonisation target": 20,
-    "Constant emission": 90,
-  },
-  {
-    Year: "2025",
-    "Baseline carbon performance of asset": 10,
-    "Sector decarbonisation target": 10,
-    "Constant emission": 90,
+    "Baseline carbon performance of asset": 75,
+    "Sector decarbonisation target": 93,
+    "Constant emission": 75,
   },
   {
     Year: "2026",
-    "Baseline carbon performance of asset": 0,
-    "Sector decarbonisation target": 5,
-    "Constant emission": 90,
+    "Baseline carbon performance of asset": 75,
+    "Sector decarbonisation target": 75,
+    "Constant emission": 75,
   },
   {
-    Year: "2027",
-    "Baseline carbon performance of asset": 0,
-    "Sector decarbonisation target": 0,
-    "Constant emission": 90,
+    Year: "2028",
+    "Baseline carbon performance of asset": 38,
+    "Sector decarbonisation target": 60,
+    "Constant emission": 75,
+  },
+  {
+    Year: "2030",
+    "Baseline carbon performance of asset": 38,
+    "Sector decarbonisation target": 50,
+    "Constant emission": 75,
+  },
+  {
+    Year: "2032",
+    "Baseline carbon performance of asset": 38,
+    "Sector decarbonisation target": 38,
+    "Constant emission": 75,
+  },
+  {
+    Year: "2034",
+    "Baseline carbon performance of asset": 20,
+    "Sector decarbonisation target": 30,
+    "Constant emission": 75,
+  },
+  {
+    Year: "2036",
+    "Baseline carbon performance of asset": 20,
+    "Sector decarbonisation target": 28,
+    "Constant emission": 75,
+  },
+  {
+    Year: "2038",
+    "Baseline carbon performance of asset": 20,
+    "Sector decarbonisation target": 22,
+    "Constant emission": 75,
+  },
+  {
+    Year: "2040",
+    "Baseline carbon performance of asset": 20,
+    "Sector decarbonisation target": 20,
+    "Constant emission": 75,
+  },
+  {
+    Year: "2042",
+    "Baseline carbon performance of asset": 5,
+    "Sector decarbonisation target": 15,
+    "Constant emission": 75,
+  },
+  {
+    Year: "2044",
+    "Baseline carbon performance of asset": 5,
+    "Sector decarbonisation target": 10,
+    "Constant emission": 75,
+  },
+  {
+    Year: "2046",
+    "Baseline carbon performance of asset": 5,
+    "Sector decarbonisation target": 6,
+    "Constant emission": 75,
+  },
+  {
+    Year: "2048",
+    "Baseline carbon performance of asset": 5,
+    "Sector decarbonisation target": 5,
+    "Constant emission": 75,
+  },
+  {
+    Year: "2050",
+    "Baseline carbon performance of asset": 5,
+    "Sector decarbonisation target": 5,
+    "Constant emission": 75,
   },
 ];
 
@@ -90,11 +138,11 @@ export const StrandingDiagram = () => {
         <Tooltip />
         <Legend />
         <Area
-          type="monotone"
           dataKey="Baseline carbon performance of asset"
-          stroke="#A2A3A5"
+          stroke="#343434"
           strokeWidth={2}
           fill="transparent"
+          type="stepBefore"
         />
         <Area
           type="monotone"
@@ -105,11 +153,11 @@ export const StrandingDiagram = () => {
           fill="url(#colorPv)"
         />
         <Area
-          type="monotone"
           dataKey="Constant emission"
           stroke="#1E1E1E"
           strokeWidth={1}
           fill="transparent"
+          strokeDasharray="4 4"
         />
       </AreaChart>
     </div>
