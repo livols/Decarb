64f1fb8db5df084d70c29d0b9ab79f474b778dc5
Stefan Harabagiu <stefan.harabagiu@gmail.com>
2022-11-01 14:27:24 +0100

    First map version

--- src/pages/product.tsx
+++ src/pages/product.tsx
@@ -1,7 +1,13 @@
+import SimpleMap from "../visualisation/map";
+
 export function Product() {
   return (
-    <div>
+    <>
+    <div style={{height: '1500px'}}>
       <h2>Product</h2>
+      <SimpleMap/>
     </div>
+
+    </>
   );
 }
