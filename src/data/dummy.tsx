c17dc733881b5e0b363287229568833316aa415d
livolsen <livolsen@live.com>
2022-11-03 22:05:39 +0100

    Moved data to another file, added input field for global warming target and added positioning of elements

--- src/data/dummy.tsx
+++ src/data/dummy.tsx
@@ -1,88 +1,187 @@
+export const strandingData = [
+  {
+    Year: "2020",
+    "Baseline carbon performance of asset": 75,
+    "Sector decarbonisation target": 95,
+    "Constant emission": 75,
+  },
+  {
+    Year: "2022",
+    "Baseline carbon performance of asset": 75,
+    "Sector decarbonisation target": 98,
+    "Constant emission": 75,
+  },
+  {
+    Year: "2024",
+    "Baseline carbon performance of asset": 75,
+    "Sector decarbonisation target": 93,
+    "Constant emission": 75,
+  },
+  {
+    Year: "2026",
+    "Baseline carbon performance of asset": 75,
+    "Sector decarbonisation target": 75,
+    "Constant emission": 75,
+  },
+  {
+    Year: "2028",
+    "Baseline carbon performance of asset": 38,
+    "Sector decarbonisation target": 60,
+    "Constant emission": 75,
+  },
+  {
+    Year: "2030",
+    "Baseline carbon performance of asset": 38,
+    "Sector decarbonisation target": 50,
+    "Constant emission": 75,
+  },
+  {
+    Year: "2032",
+    "Baseline carbon performance of asset": 38,
+    "Sector decarbonisation target": 38,
+    "Constant emission": 75,
+  },
+  {
+    Year: "2034",
+    "Baseline carbon performance of asset": 20,
+    "Sector decarbonisation target": 30,
+    "Constant emission": 75,
+  },
+  {
+    Year: "2036",
+    "Baseline carbon performance of asset": 20,
+    "Sector decarbonisation target": 28,
+    "Constant emission": 75,
+  },
+  {
+    Year: "2038",
+    "Baseline carbon performance of asset": 20,
+    "Sector decarbonisation target": 22,
+    "Constant emission": 75,
+  },
+  {
+    Year: "2040",
+    "Baseline carbon performance of asset": 20,
+    "Sector decarbonisation target": 20,
+    "Constant emission": 75,
+  },
+  {
+    Year: "2042",
+    "Baseline carbon performance of asset": 5,
+    "Sector decarbonisation target": 15,
+    "Constant emission": 75,
+  },
+  {
+    Year: "2044",
+    "Baseline carbon performance of asset": 5,
+    "Sector decarbonisation target": 10,
+    "Constant emission": 75,
+  },
+  {
+    Year: "2046",
+    "Baseline carbon performance of asset": 5,
+    "Sector decarbonisation target": 6,
+    "Constant emission": 75,
+  },
+  {
+    Year: "2048",
+    "Baseline carbon performance of asset": 5,
+    "Sector decarbonisation target": 5,
+    "Constant emission": 75,
+  },
+  {
+    Year: "2050",
+    "Baseline carbon performance of asset": 5,
+    "Sector decarbonisation target": 5,
+    "Constant emission": 75,
+  },
+];
+
 export const addressData = [
   {
-    "address": "gothersgade",
-    "number": "3",
-    "code": "1123",
-    "city": "københavn k",
-    "expenditure": [
+    address: "gothersgade",
+    number: "3",
+    code: "1123",
+    city: "københavn k",
+    expenditure: [
       {
-        "year": 2022,
+        year: 2022,
         "With Improvement": 100,
         "Without Improvement": 120,
       },
       {
-        "year": 2023,
+        year: 2023,
         "With Improvement": 182,
         "Without Improvement": 286,
       },
       {
-        "year": 2024,
+        year: 2024,
         "With Improvement": 148,
         "Without Improvement": 200,
       },
       {
-        "year": 2025,
+        year: 2025,
         "With Improvement": 119,
         "Without Improvement": 158,
       },
       {
-        "year": 2026,
+        year: 2026,
         "With Improvement": 100,
         "Without Improvement": 142,
       },
       {
-        "year": 2027,
+        year: 2027,
         "With Improvement": 83,
         "Without Improvement": 146,
       },
       {
-        "year": 2028,
+        year: 2028,
         "With Improvement": 56,
-        "Without Improvement": 140
-      }
-    ]
+        "Without Improvement": 140,
+      },
+    ],
   },
   {
-    "address": "gothersgade",
-    "number": "10",
-    "code": "1123",
-    "city": "københavn k",
-    "expenditure": [
+    address: "gothersgade",
+    number: "10",
+    code: "1123",
+    city: "københavn k",
+    expenditure: [
       {
-        "year": 2022,
+        year: 2022,
         "With Improvement": 251,
         "Without Improvement": 295,
       },
       {
-        "year": 2023,
+        year: 2023,
         "With Improvement": 223,
         "Without Improvement": 286,
       },
       {
-        "year": 2024,
+        year: 2024,
         "With Improvement": 200,
         "Without Improvement": 254,
       },
       {
-        "year": 2025,
+        year: 2025,
         "With Improvement": 174,
         "Without Improvement": 235,
       },
       {
-        "year": 2026,
+        year: 2026,
         "With Improvement": 152,
         "Without Improvement": 252,
       },
       {
-        "year": 2027,
+        year: 2027,
         "With Improvement": 110,
         "Without Improvement": 230,
       },
       {
-        "year": 2028,
+        year: 2028,
         "With Improvement": 84,
-        "Without Improvement": 140
-      }
-    ]
-  }
-]
+        "Without Improvement": 140,
+      },
+    ],
+  },
+];
--- src/visualisation/strandingDiagram.tsx
+++ src/visualisation/strandingDiagram.tsx
@@ -1,4 +1,4 @@
-import "../styles/result.css";
+import React from "react";
 import {
   AreaChart,
   CartesianGrid,
@@ -8,158 +8,107 @@ import {
   XAxis,
   YAxis,
 } from "recharts";
-
-const data = [
-  {
-    Year: "2020",
-    "Baseline carbon performance of asset": 75,
-    "Sector decarbonisation target": 95,
-    "Constant emission": 75,
-  },
-  {
-    Year: "2022",
-    "Baseline carbon performance of asset": 75,
-    "Sector decarbonisation target": 98,
-    "Constant emission": 75,
-  },
-  {
-    Year: "2024",
-    "Baseline carbon performance of asset": 75,
-    "Sector decarbonisation target": 93,
-    "Constant emission": 75,
-  },
-  {
-    Year: "2026",
-    "Baseline carbon performance of asset": 75,
-    "Sector decarbonisation target": 75,
-    "Constant emission": 75,
-  },
-  {
-    Year: "2028",
-    "Baseline carbon performance of asset": 38,
-    "Sector decarbonisation target": 60,
-    "Constant emission": 75,
-  },
-  {
-    Year: "2030",
-    "Baseline carbon performance of asset": 38,
-    "Sector decarbonisation target": 50,
-    "Constant emission": 75,
-  },
-  {
-    Year: "2032",
-    "Baseline carbon performance of asset": 38,
-    "Sector decarbonisation target": 38,
-    "Constant emission": 75,
-  },
-  {
-    Year: "2034",
-    "Baseline carbon performance of asset": 20,
-    "Sector decarbonisation target": 30,
-    "Constant emission": 75,
-  },
-  {
-    Year: "2036",
-    "Baseline carbon performance of asset": 20,
-    "Sector decarbonisation target": 28,
-    "Constant emission": 75,
-  },
-  {
-    Year: "2038",
-    "Baseline carbon performance of asset": 20,
-    "Sector decarbonisation target": 22,
-    "Constant emission": 75,
-  },
-  {
-    Year: "2040",
-    "Baseline carbon performance of asset": 20,
-    "Sector decarbonisation target": 20,
-    "Constant emission": 75,
-  },
-  {
-    Year: "2042",
-    "Baseline carbon performance of asset": 5,
-    "Sector decarbonisation target": 15,
-    "Constant emission": 75,
-  },
-  {
-    Year: "2044",
-    "Baseline carbon performance of asset": 5,
-    "Sector decarbonisation target": 10,
-    "Constant emission": 75,
-  },
-  {
-    Year: "2046",
-    "Baseline carbon performance of asset": 5,
-    "Sector decarbonisation target": 6,
-    "Constant emission": 75,
-  },
-  {
-    Year: "2048",
-    "Baseline carbon performance of asset": 5,
-    "Sector decarbonisation target": 5,
-    "Constant emission": 75,
-  },
-  {
-    Year: "2050",
-    "Baseline carbon performance of asset": 5,
-    "Sector decarbonisation target": 5,
-    "Constant emission": 75,
-  },
-];
+import { FormControl, MenuItem, Typography, Box } from "@mui/material";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+import { strandingData } from "../data/dummy";
+import "@fontsource/inter";
+import "@fontsource/lexend";
 
 export const StrandingDiagram = () => {
+  const [age, setAge] = React.useState("1");
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setAge(event.target.value);
+  };
   return (
-    <div className="plot">
-      <AreaChart
-        width={1052}
-        height={269}
-        data={data}
-        margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+    <div>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+        }}
       >
-        <defs>
-          <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
-            <stop offset="5%" stopColor="#7A9C6C" stopOpacity={0.8} />
-            <stop offset="95%" stopColor="#7A9C6C" stopOpacity={0} />
-          </linearGradient>
-        </defs>
-        <XAxis dataKey="Year" stroke="#A2A3A5" />
-        <YAxis
-          label={{
-            value: "GHG Intensity [KgCO2e/m2/yr]",
-            angle: -90,
-            position: "center",
-            dx: -40,
-            fill: "#A2A3A5",
-          }}
-          stroke="#A2A3A5"
-        />
-        <CartesianGrid vertical={false} />
-        <Tooltip />
-        <Legend />
-        <Area
-          dataKey="Baseline carbon performance of asset"
-          stroke="#343434"
-          strokeWidth={2}
-          fill="transparent"
-          type="stepBefore"
-        />
-        <Area
-          type="monotone"
-          dataKey="Sector decarbonisation target"
-          stroke="#7A9C6C"
-          fillOpacity={1}
-          strokeWidth={2}
-          fill="url(#colorPv)"
-        />
-        <Area
-          dataKey="Constant emission"
-          stroke="#1E1E1E"
-          strokeWidth={1}
-          fill="transparent"
-          strokeDasharray="4 4"
-        />
-      </AreaChart>
+        <div style={{ paddingTop: "10px", paddingBottom: "40px" }}>
+          <Typography
+            sx={{ color: "#0D0D0D", fontFamily: "Lexend", fontWeight: "bold" }}
+            variant="h4"
+          >
+            PFA property portfolio
+          </Typography>
+        </div>
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <div>
+          <AreaChart
+            width={1052}
+            height={269}
+            data={strandingData}
+            margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+          >
+            <defs>
+              <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+                <stop offset="5%" stopColor="#7A9C6C" stopOpacity={0.8} />
+                <stop offset="95%" stopColor="#7A9C6C" stopOpacity={0} />
+              </linearGradient>
+            </defs>
+            <XAxis dataKey="Year" stroke="#A2A3A5" />
+            <YAxis
+              label={{
+                value: "GHG Intensity [KgCO2e/m2/yr]",
+                angle: -90,
+                position: "center",
+                dx: -40,
+                fill: "#A2A3A5",
+              }}
+              stroke="#A2A3A5"
+            />
+            <CartesianGrid vertical={false} />
+            <Tooltip />
+            <Legend />
+            <Area
+              dataKey="Baseline carbon performance of asset"
+              stroke="#343434"
+              strokeWidth={2}
+              fill="transparent"
+              type="stepBefore"
+            />
+            <Area
+              type="monotone"
+              dataKey="Sector decarbonisation target"
+              stroke="#7A9C6C"
+              fillOpacity={1}
+              strokeWidth={2}
+              fill="url(#colorPv)"
+            />
+            <Area
+              dataKey="Constant emission"
+              stroke="#1E1E1E"
+              strokeWidth={1}
+              fill="transparent"
+              strokeDasharray="4 4"
+            />
+          </AreaChart>
+        </div>
+        <div style={{ paddingTop: "65px" }}>
+          <FormControl sx={{ m: 1, minWidth: 50 }}>
+            <Typography
+              sx={{ color: "#0D0D0D", fontFamily: "Inter" }}
+              variant="caption"
+            >
+              Global warming target
+            </Typography>
+            <Select value={age} onChange={handleChange} displayEmpty>
+              <MenuItem value={1}>1.5°C</MenuItem>
+              <MenuItem value={2}>2°C</MenuItem>
+            </Select>
+          </FormControl>
+        </div>
+      </Box>
     </div>
   );
 };
