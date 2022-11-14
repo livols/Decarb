7b73bfb489f37d56fb1d6c4b0f3c9b04685d8eb6
livolsen <livolsen@live.com>
2022-11-14 23:22:02 +0100

    List component

---
+++ src/utils/assetList.tsx
@@ -0,0 +1,49 @@
+import * as React from "react";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import { Card, CardContent, Divider, Typography } from "@mui/material";
+import "@fontsource/inter";
+
+export function AssetList() {
+  const properties = [
+    { id: 0, name: "Asset" },
+    { id: 1, name: "Asset" },
+    { id: 2, name: "Asset" },
+    { id: 3, name: "Asset" },
+    { id: 4, name: "Asset" },
+  ];
+  return (
+    <Card sx={{ maxWidth: 250 }}>
+      <CardContent>
+        <Typography
+          sx={{
+            color: "#0D0D0D",
+            fontFamily: "Inter",
+            fontWeight: "bold",
+            textAlign: "center",
+            paddingBottom: "8px",
+          }}
+          variant="body2"
+        >
+          Bottom 5 properties
+        </Typography>
+        <Divider />
+        <List dense sx={{ maxWidth: 250 }}>
+          {properties.map(({ id, name }) => {
+            const labelId = `label${id}`;
+            return (
+              <ListItem key={id} disablePadding>
+                <ListItemButton>
+                  <ListItemText>{`${id + 1}.`}</ListItemText>
+                  <ListItemText id={labelId} primary={`${name}`} />
+                </ListItemButton>
+              </ListItem>
+            );
+          })}
+        </List>
+      </CardContent>
+    </Card>
+  );
+}
