6694018ddfaaf54133b3f4b8e9c46afefb9273eb
Stefan Harabagiu <stefan.harabagiu@gmail.com>
2022-11-17 17:18:58 +0100

    frontpage

--- src/pages/product.tsx
+++ src/pages/product.tsx
@@ -1,7 +1,41 @@
+import Squiggle from "../images/squiggle.svg";
+import pfalogo from "../images/pfa-ejendomme.svg";
+
 export function Product() {
   return (
-    <div style={{marginLeft: "2%"}}>
-      <h2>Product</h2>
-    </div>
+    <>
+      <div className="frontpage-content">
+        <div className="title-card">
+          <div className="title">
+            <span>Translating CO2 emissions targets into </span>{" "}
+            <span className="action">action</span>
+            <img className="squiggle" src={Squiggle}></img>
+          </div>
+        </div>
+        <div className="frontpage-about">
+          <div className="about1">What we do</div>
+          <div className="about2">
+            Our solution is enabling the decarbonization of real estate market
+            through operationalization of emissions targets. Powered by data, AI
+            and scientific research, we are translating net-zero goals of the
+            Paris Agreement, EU legislation and local government legislation
+            into actionable analytics that cover a wide range of decarbonization
+            pathways of real estate.
+          </div>
+          <div className="about3">Customer stories</div>
+          <div className="stories">
+            <img className="PFALogo" src={pfalogo}></img>
+            <span className="pfaText">
+              Lorem ipsum dolor sit amet. Ea odio porro est repellendus dolores
+              aut atque error in dolorem iste. Qui perspiciatis repudiandae nam
+              quisquam iure vel molestiae accusamus est nihil perferendis et
+              possimus quidem aut temporibus dolores qui rerum dolores. Sed
+              perspiciatis explicabo sit rerum atque et sunt cumque sed magni
+              omnis!
+            </span>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
